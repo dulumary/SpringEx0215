5623cf95215d83f6f02ebaa95bf914906729a391
kim <kim@naver.com>
2022-02-16 17:59:18 +0900

    [김인규]  - 데이터 베이스 연동 예제

---
+++ src/main/java/com/marondal/ex/lesson02/dao/UsedGoodsDAO.java
@@ -0,0 +1,13 @@
+package com.marondal.ex.lesson02.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.marondal.ex.lesson02.model.UsedGoods;
+
+@Repository
+public interface UsedGoodsDAO {
+	
+	public List<UsedGoods> selectUsedGoodsList();
+}
