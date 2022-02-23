e32bbe4c30ba4fd6fce050df28f76309562ba7de
kim <kim@naver.com>
2022-02-23 17:48:50 +0900

    [김인규]  - jstl 반복문 예제 일부 추 가

--- src/main/java/com/marondal/ex/lesson05/Lesson05Controller.java
+++ src/main/java/com/marondal/ex/lesson05/Lesson05Controller.java
@@ -1,13 +1,35 @@
 package com.marondal.ex.lesson05;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/lesson05")
 public class Lesson05Controller {
 	
-	@GetMapping("/lesson05/ex01")
+	@GetMapping("/ex01")
 	public String ex01() {
 		return "lesson05/ex01";
 	}
+	
+	@GetMapping("/ex02")
+	public String ex02(Model model) {
+		
+		// 과일이름 리스트
+		List<String> fruits = new ArrayList<>();
+		fruits.add("apple");
+		fruits.add("banana");
+		fruits.add("mango");
+		
+		model.addAttribute("fruits", fruits);
+		
+		
+		return "lesson05/ex02";
+	}
+	
 }
